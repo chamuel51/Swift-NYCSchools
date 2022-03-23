a50459c209b5b790452546879e269c83e58b9697
Miguel Jimenez <chamuel.developer@gmail.com>
2022-03-23 13:14:54 -0400

    Initial Commit

---
+++ 20220323-MiguelJimenez-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20220323-MiguelJimenez-NYCSchools
+//
+//  Created by chamuel castillo on 3/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
